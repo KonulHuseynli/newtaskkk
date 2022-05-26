d62cb8b84b75b95d7998e7ed89ee1421169d5bb0
Konul R. Huseynli <konulrh@code.local>
2022-05-26 18:54:20 +0400

    added new task

---
+++ carnew.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    class car
+    {
+        public string Model { get; set; }
+
+        public string Brand { get; set; }
+
+        private int _probek;
+        public int Probek
+        {
+            get
+            {
+                return _probek;
+            }
+
+
+            set
+            {
+                int temp = value;
+                int reverse = 0;
+                while (value > 0)
+                {
+                    reverse = reverse * 10 + value % 10;
+                    value = value / 10;
+                }
+                if (temp == reverse)
+                {
+                    _probek = temp;
+                }
+                else
+                {
+                    _probek = 0;
+                }
+            }
+
+        }
+    }
+}
+}
---
+++ prognew.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car();
+
+
+            car.Probek = 225;
+
+            Console.WriteLine(car.Probek);
+
+
+            car.Probek = 2222;
+            Console.WriteLine(car.Probek);
+        }
+    }
+}
